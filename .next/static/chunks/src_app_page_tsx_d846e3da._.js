ad23ee597349cb66f331e20bd4c83b2f72880123
vanshtomar06 <140646276+vanshtomar06@users.noreply.github.com>
2025-08-25 17:01:00 +0530

    Add files via upload

---
+++ .next/static/chunks/src_app_page_tsx_d846e3da._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_961dea51._.js",
+  "static/chunks/node_modules_lodash_f240f67a._.js",
+  "static/chunks/node_modules_recharts_es6_8235043b._.js",
+  "static/chunks/node_modules_6f04c02b._.js"
+],
+    source: "dynamic"
+});
